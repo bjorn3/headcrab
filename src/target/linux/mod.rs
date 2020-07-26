85687e67af9d11690ad1e8e8d394434fc42a9795
Nikita Baksalyar <nikita.baksalyar@gmail.com>
2020-07-26 22:09:07 +0100

    Move ReadMemory struct into a separate module

--- src/target/linux/mod.rs
+++ src/target/linux/mod.rs
@@ -1,14 +1,14 @@
+mod readmem;
 mod writemem;
 
 use nix::unistd::{getpid, Pid};
 use std::{
     fs::File,
     io::{BufRead, BufReader},
-    marker::PhantomData,
-    mem,
 };
 
 use crate::target::unix::{self, UnixTarget};
+pub use readmem::ReadMemory;
 pub use writemem::WriteMemory;
 
 /// This structure holds the state of a debuggee on Linux based systems
@@ -55,111 +55,6 @@ impl LinuxTarget {
     /// Reads the register values from the main thread of a debuggee process.
     pub fn read_regs(&self) -> Result<libc::user_regs_struct, Box<dyn std::error::Error>> {
         nix::sys::ptrace::getregs(self.pid()).map_err(|err| err.into())
-    }
-}
-
-/// A single memory read operation.
-struct ReadOp {
-    // Remote memory location.
-    remote_base: usize,
-    // Size of the `local_ptr` buffer.
-    len: usize,
-    // Pointer to a local destination buffer.
-    local_ptr: *mut libc::c_void,
-}
-
-impl ReadOp {
-    /// Converts the memory read operation into a remote IoVec.
-    fn as_remote_iovec(&self) -> libc::iovec {
-        libc::iovec {
-            iov_base: self.remote_base as *const libc::c_void as *mut _,
-            iov_len: self.len,
-        }
-    }
-
-    /// Converts the memory read operation into a local IoVec.
-    fn as_local_iovec(&self) -> libc::iovec {
-        libc::iovec {
-            iov_base: self.local_ptr,
-            iov_len: self.len,
-        }
-    }
-}
-
-/// Allows to read memory from different locations in debuggee's memory as a single operation.
-/// On Linux, this will correspond to a single system call / context switch.
-pub struct ReadMemory<'a> {
-    pid: Pid,
-    read_ops: Vec<ReadOp>,
-    _marker: PhantomData<&'a mut ()>,
-}
-
-impl<'a> ReadMemory<'a> {
-    fn new(pid: Pid) -> Self {
-        ReadMemory {
-            pid,
-            read_ops: Vec::new(),
-            _marker: PhantomData,
-        }
-    }
-
-    /// Reads a value of type `T` from debuggee's memory at location `remote_base`.
-    /// This value will be written to the provided variable `val`.
-    /// You should call `apply` in order to execute the memory read operation.
-    /// The provided variable `val` can't be accessed until either `apply` is called or `self` is
-    /// dropped.
-    ///
-    /// # Safety
-    ///
-    /// The type `T` must not have any invalid values.
-    /// For example `T` must not be a `bool`, as `transmute::<u8, bool>(2)` is not a valid value for a bool.
-    /// In case of doubt, wrap the type in [`mem::MaybeUninit`].
-    // todo: further document mem safety - e.g., what happens in the case of partial read
-    pub unsafe fn read<T>(mut self, val: &'a mut T, remote_base: usize) -> Self {
-        self.read_ops.push(ReadOp {
-            remote_base,
-            len: mem::size_of::<T>(),
-            local_ptr: val as *mut T as *mut libc::c_void,
-        });
-
-        self
-    }
-
-    /// Executes the memory read operation.
-    pub fn apply(self) -> Result<(), Box<dyn std::error::Error>> {
-        // Create a list of `IoVec`s and remote `IoVec`s
-        let remote_iov = self
-            .read_ops
-            .iter()
-            .map(ReadOp::as_remote_iovec)
-            .collect::<Vec<_>>();
-
-        let local_iov = self
-            .read_ops
-            .iter()
-            .map(ReadOp::as_local_iovec)
-            .collect::<Vec<_>>();
-
-        let bytes_read = unsafe {
-            // todo: document unsafety
-            libc::process_vm_readv(
-                self.pid.into(),
-                local_iov.as_ptr(),
-                local_iov.len() as libc::c_ulong,
-                remote_iov.as_ptr(),
-                remote_iov.len() as libc::c_ulong,
-                0,
-            )
-        };
-
-        if bytes_read == -1 {
-            // fixme: return a proper error type
-            return Err(Box::new(nix::Error::last()));
-        }
-
-        // fixme: check that it's an expected number of read bytes and account for partial reads
-
-        Ok(())
     }
 }
 
