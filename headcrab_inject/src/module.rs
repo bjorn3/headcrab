bd63a3062c1cbd900a99c2dfdf1addefa2241f79
bjorn3 <bjorn3@users.noreply.github.com>
2020-10-01 11:22:24 +0200

    Update cranelift

--- headcrab_inject/src/module.rs
+++ headcrab_inject/src/module.rs
@@ -116,7 +116,7 @@ impl<'a> InjectionModule<'a> {
             &mut code_mem,
             &mut relocs,
             &mut binemit::NullTrapSink {},
-            &mut binemit::NullStackmapSink {},
+            &mut binemit::NullStackMapSink {},
         )
         .unwrap();
 
